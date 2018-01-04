a1adeab4bb2f17e4493ee8b5a336ab20691d8e2d
Johannes Baiter <johannes.baiter@gmail.com>
2018-01-04 22:09:55 +0100

    Clean up test infrastructure

--- tests/conftest.py
+++
@@ -1,6 +0,0 @@
-from util import MemoryUsage
-
-
-def pytest_assertrepr_compare(op, left, right):
-    if isinstance(left, MemoryUsage) and isinstance(right, MemoryUsage) and op == ">=":
-        return ['Detected memory leak: {} bytes'.format(right._rss - left._rss)]
